bugfix : Do not average lstm last layer

src/inputs/util.py
@@ -1,10 +1,9 @@
 import os
-import re
 import random
+import re
+
 import numpy as np
 import tensorflow as tf
-from collections import defaultdict
-from collections import namedtuple
 
 flags = tf.app.flags
 
@@ -100,6 +99,16 @@ def get_vocab_file():
 
 def data_dir():
   return "{}-{}k".format(FLAGS.data_dir, FLAGS.vocab_size)
+
+
+def get_logdir():
+  return "{}-{}-{}-{}-{}-{}-{}/".format(FLAGS.logdir,
+                                        FLAGS.batch_size,
+                                        FLAGS.hidden_size,
+                                        FLAGS.vocab_size,
+                                        FLAGS.keep_prob,
+                                        FLAGS.num_epochs,
+                                        FLAGS.lrn_rate)
 
 def write_vocab(vocab, vocab_file=get_vocab_file()):
   '''write vocab to the file

src/main.py
@@ -14,7 +14,7 @@ from termcolor import cprint
 
 from inputs import fudan
 from inputs import util
-from inputs.util import data_dir, get_vocab_file
+from inputs.util import data_dir, get_vocab_file, get_logdir
 from models import mtl_model
 
 # tf.set_random_seed(0)
@@ -134,7 +134,7 @@ def train(sess, m_train, m_valid):
   start_time = time.time()
   orig_begin_time = start_time
 
-  summary_prefix = os.path.join(FLAGS.logdir, model_name())
+  summary_prefix = os.path.join(get_logdir(), model_name())
 
   train_writer = tf.summary.FileWriter(summary_prefix + '/train', sess.graph)
   valid_writer = tf.summary.FileWriter(summary_prefix + '/valid', sess.graph)

src/models/base_model.py
@@ -1,6 +1,9 @@
 import os
+
 import tensorflow as tf
 from tensorflow.python.framework import ops
+
+from inputs.util import get_logdir
 
 FLAGS = tf.app.flags.FLAGS
 
@@ -16,7 +19,7 @@ class BaseModel(object):
     '''
     # shared between train and valid model instance
     self.saver = tf.train.Saver(var_list=None)
-    self.save_dir = os.path.join(FLAGS.logdir, save_dir)
+    self.save_dir = os.path.join(get_logdir(), save_dir)
     tf.gfile.MakeDirs(self.save_dir)
     self.save_path = os.path.join(self.save_dir, "model.ckpt")
 

src/models/lstm.py
@@ -1,4 +1,3 @@
-from tensor2tensor.models.lstm import lstm, lstm_seq2seq
 import tensorflow as tf
 from tensorflow.contrib.seq2seq import BahdanauAttention
 
@@ -60,6 +59,6 @@ class LSTMLayer(tf.layers.Layer):
                 self.alignment = alignment
                 alignment = tf.expand_dims(alignment, 1)
                 context = tf.matmul(alignment, attention.values)
-                return tf.expand_dims(tf.reduce_mean(context, axis=[1,2]), -1)
+                return tf.squeeze(context)
 
-        return tf.expand_dims(tf.reduce_mean(output, axis=[1,2]), -1)
+        return tf.squeeze(tf.reduce_mean(output, axis=[1]))